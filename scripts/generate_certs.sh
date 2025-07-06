a1c53410dde23496b1c1de0d385c049e997a6661
shyamsundart14 <48407502+shyamsundart14@users.noreply.github.com>
2025-07-06 19:05:00 +0530

    Create generate_certs.sh

---
+++ scripts/generate_certs.sh
@@ -0,0 +1,25 @@
+export HOST0=192.168.68.13
+export HOST1=192.168.68.14
+export HOST2=192.168.68.15
+kubeadm init phase certs etcd-server --config=/tmp/${HOST2}/kubeadmcfg.yaml
+kubeadm init phase certs etcd-peer --config=/tmp/${HOST2}/kubeadmcfg.yaml
+kubeadm init phase certs etcd-healthcheck-client --config=/tmp/${HOST2}/kubeadmcfg.yaml
+kubeadm init phase certs apiserver-etcd-client --config=/tmp/${HOST2}/kubeadmcfg.yaml
+ssh root@${HOST2} "mkdir -pv /etc/kubernetes/pki/etcd"
+cp -R /etc/kubernetes/pki /tmp/${HOST2}/
+
+
+kubeadm init phase certs etcd-server --config=/tmp/${HOST1}/kubeadmcfg.yaml
+kubeadm init phase certs etcd-peer --config=/tmp/${HOST1}/kubeadmcfg.yaml
+kubeadm init phase certs etcd-healthcheck-client --config=/tmp/${HOST1}/kubeadmcfg.yaml
+kubeadm init phase certs apiserver-etcd-client --config=/tmp/${HOST1}/kubeadmcfg.yaml
+ssh root@${HOST1} "mkdir -pv /etc/kubernetes/pki/etcd"
+cp -R /etc/kubernetes/pki /tmp/${HOST1}/
+
+
+kubeadm init phase certs etcd-server --config=/tmp/${HOST0}/kubeadmcfg.yaml
+kubeadm init phase certs etcd-peer --config=/tmp/${HOST0}/kubeadmcfg.yaml
+kubeadm init phase certs etcd-healthcheck-client --config=/tmp/${HOST0}/kubeadmcfg.yaml
+kubeadm init phase certs apiserver-etcd-client --config=/tmp/${HOST0}/kubeadmcfg.yaml
+ssh root@${HOST0} "mkdir -pv /etc/kubernetes/pki/etcd"
+cp -R /etc/kubernetes/pki /tmp/${HOST0}/
